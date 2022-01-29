4305dbdb760e3a7405fdab2fa0b4738e9c8b108e
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-01-29 16:03:30 -0500

    Use sass for styling, fix footer link address

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -3,7 +3,9 @@ const Footer = () => {
 		<footer className="footer">
 			<ul className="footer__list text-muted">
 				<li className="footer__list-item">
-					<a href="https://github.com/sprioleau/deekay-drops-remix">source</a>
+					<a href="https://github.com/sprioleau/remix-starter-deekay-drops" target="_blank" rel="noreferrer">
+						source
+					</a>
 				</li>
 				<li className="footer__list-item">
 					created by <a href="https://github.com/sprioleau">@sprioleau</a>
