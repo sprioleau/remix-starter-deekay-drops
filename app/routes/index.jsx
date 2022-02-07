e84d55043c14fa41254662af6592ea6c94c7ddd0
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2022-02-06 21:03:01 -0500

    Use newly created util for scraping DeeKay website for image URLs

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,15 +1,17 @@
-import Card from "../components/Card";
-import Layout from "../components/Layout";
+import { Link, useLoaderData } from "remix";
+
+import Card from "~/components/Card";
+import Layout from "~/components/Layout";
 import cardStyles from "~/styles/components/card.css";
-import { expandImageMeta } from "../utils/expandImageMeta";
+import { expandImageMeta } from "~/utils/expandImageMeta";
 import footerStyles from "~/styles/components/footer.css";
+import getDrops from "~/utils/getDrops";
 import headerStyles from "~/styles/components/header.css";
 import indexStyles from "~/styles/styles.css";
-import { useLoaderData } from "remix";
 
 export const loader = async () => {
-	const response = await fetch("https://deekay-drops.netlify.app/api/drops");
-	const images = await response.json();
+	const { images, error } = await getDrops();
+	if (error) return null;
 	return expandImageMeta(images);
 };
 
@@ -29,11 +31,17 @@ export default function Index() {
 	return (
 		<Layout>
 			<section>
-				<ul className="grid" style={{ gridTemplateColumns: `repeat(auto-fit, minmax(${imageWidth}px, 1fr)` }}>
-					{drops.map((drop) => (
-						<Card key={drop.alt} drop={drop} imageWidth={imageWidth} />
-					))}
-				</ul>
+				{drops ? (
+					<ul className="grid" style={{ gridTemplateColumns: `repeat(auto-fit, minmax(${imageWidth}px, 1fr)` }}>
+						{drops.map((drop) => (
+							<Card key={drop.alt} drop={drop} imageWidth={imageWidth} />
+						))}
+					</ul>
+				) : (
+					<p>
+						Uh oh, something went wrong. <Link to="/">Try again.</Link>
+					</p>
+				)}
 			</section>
 		</Layout>
 	);
